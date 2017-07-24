1cd3a24fdd0cad731094147b93488e504395c3c9
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-07-24 16:54:54 -0700

    completed project product management assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Product } from './product'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import { ProductListService } from './product-list.service'
+import { Subscription } from 'rxjs/Subscription'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'PPM - Project Product Management';
+  productList: Array<Product> = [];
+  nextProductId = 1;
+  subscription: Subscription;
+  otherSubscription: Subscription;
+  constructor(private _productListService: ProductListService){
+    _productListService.updateProducts(this.productList)
+    this.subscription = _productListService.observedProductList.subscribe(
+      (updatedProductList)=>{
+        this.productList = updatedProductList;
+      },
+      (err)=>{},
+      ()=>{}
+    )
+    _productListService.updateNextProductId(this.nextProductId)
+    this.otherSubscription = _productListService.observedNextProductId.subscribe(
+      (updatedNextProductId)=>{
+        this.nextProductId = updatedNextProductId;
+      },
+      (err)=>{},
+      ()=>{}
+    )
+  };
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+    this.otherSubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ProductListService } from './product-list.service';
+
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './list/edit/edit.component';
+import { NewComponent } from './list/new/new.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    NewComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ProductListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
